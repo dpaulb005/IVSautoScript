dc37099531c4b56ccca0d04c5acf793f1ac7826b
dpaulb005 <69474823+dpaulb005@users.noreply.github.com>
2020-08-20 13:14:13 -0400

    Add files via upload

--- checkChange.py
+++ checkChange.py
@@ -4,12 +4,12 @@ import boto3
 import sys
 import smtplib, ssl
 import time
-def email(old_status, active_data,ip_encode):
+def email(old_status, active_data,ip_encode,passwordGmail):
     port = 587  # For starttls
     smtp_server = "smtp.gmail.com"
     sender_email = "david.brown@videon-central.com"
     receiver_email = "dpb23@scasd.org"
-    password = 'PaulChase123'
+    password = passwordGmail
     message = """\
     Subject: Stream has changed status's
 
@@ -24,20 +24,22 @@ def email(old_status, active_data,ip_encode):
         server.sendmail(sender_email, receiver_email, message)
 
 old_status = '"STARTING"'
-def main(old_status,ip_encode):
+def main(old_status,ip_encode,passwordGmail):
     while True:
         encoder_data = restful.get_encoders(ip_encode)
         status_data = encoder_data['vid_encoders']
         active_data = json.dumps(status_data[0]["status"])
         print(active_data)
         if active_data != old_status:
-            email(old_status, active_data,ip_encode)
+            email(old_status, active_data,ip_encode,passwordGmail)
             old_status = active_data
         time.sleep(10)
 if __name__ == "__main__":
-    if len(sys.argv) <1:
+    if len(sys.argv) <2:
         print("%s:  Error: %s\n" % (sys.argv[0], "Not enough command options given"))
         print("Argument 1 (required): IP of stream")
+        print('Argument 2 (required): Password of gmail account')
     else:
         ip_encode = sys.argv[1]
-main('"STARTING"', ip_encode)
+        passwordGmail = sys.argv[2]
+main('"STARTING"', ip_encode,passwordGmail)
